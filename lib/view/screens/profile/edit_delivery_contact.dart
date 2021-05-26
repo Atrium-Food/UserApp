1eb7cd77e70ac1fe39a5b98dc505a74ef843855a
Manasa PVS <venkatasrimanasa.p18@iiits.in>
2021-05-26 10:55:32 +0530

    missing screens

---
+++ lib/view/screens/profile/edit_delivery_contact.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class DeliveryContactScreen extends StatefulWidget {
+  @override
+  _DeliveryContactScreenState createState() => _DeliveryContactScreenState();
+}
+
+class _DeliveryContactScreenState extends State<DeliveryContactScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+
+    );
+  }
+}
