21d2f7d682892fb6be3a5577ba734b2b42836960
nikhil <ngaur6834@gmail.com>
2021-06-09 14:55:26 +0530

    google signIn welcome Screen

--- lib/view/screens/welcome_screen/welcome_screen.dart
+++ lib/view/screens/welcome_screen/welcome_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_restaurant/localization/language_constrants.dart';
+import 'package:flutter_restaurant/provider/auth_provider.dart';
 import 'package:flutter_restaurant/utill/color_resources.dart';
 import 'package:flutter_restaurant/utill/dimensions.dart';
 import 'package:flutter_restaurant/utill/images.dart';
@@ -9,64 +10,103 @@ import 'package:flutter_restaurant/view/screens/auth/create_account_screen.dart'
 import 'package:flutter_restaurant/view/screens/auth/login_screen.dart';
 import 'package:flutter_restaurant/view/screens/auth/signup_screen.dart';
 import 'package:flutter_restaurant/view/screens/dashboard/dashboard_screen.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:provider/provider.dart';
 
 class WelcomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: ListView(
-        children: [
-          SizedBox(height: 50),
-          Container(
-              alignment: Alignment.bottomCenter,
-              padding: EdgeInsets.all(30),
-              child: Image.asset(Images.efood_bike_with_grasp, height: 200)),
-          SizedBox(height: 30),
-          Text(
-            getTranslated('welcome', context),
-            textAlign: TextAlign.center,
-            style: Theme.of(context).textTheme.headline3.copyWith(
-                color: Theme.of(context).textTheme.bodyText1.color,
-                fontSize: 32),
-          ),
-          Padding(
-            padding: const EdgeInsets.all(Dimensions.PADDING_SIZE_DEFAULT),
-            child: Text(
-              getTranslated('welcome_to_efood', context),
+      body: Consumer<AuthProvider>(
+        builder: (context, authProvider, child) => ListView(
+          children: [
+            SizedBox(height: 50),
+            Container(
+                alignment: Alignment.bottomCenter,
+                padding: EdgeInsets.all(30),
+                child: Image.asset(Images.efood_bike_with_grasp, height: 200)),
+            SizedBox(height: 30),
+            Text(
+              getTranslated('welcome', context),
               textAlign: TextAlign.center,
-              style: Theme.of(context)
-                  .textTheme
-                  .headline2
-                  .copyWith(color: ColorResources.getGreyColor(context)),
+              style: Theme.of(context).textTheme.headline3.copyWith(
+                  color: Theme.of(context).textTheme.bodyText1.color,
+                  fontSize: 32),
             ),
-          ),
-          SizedBox(height: 50),
-          Padding(
-            padding: const EdgeInsets.all(Dimensions.PADDING_SIZE_DEFAULT),
-            child: CustomButton(
-              btnTxt: getTranslated('login', context),
-              onTap: () {
-                Navigator.of(context).pushReplacement(
-                    MaterialPageRoute(builder: (_) => LoginScreen()));
-              },
+            Padding(
+              padding: const EdgeInsets.all(Dimensions.PADDING_SIZE_DEFAULT),
+              child: Text(
+                getTranslated('welcome_to_efood', context),
+                textAlign: TextAlign.center,
+                style: Theme.of(context)
+                    .textTheme
+                    .headline2
+                    .copyWith(color: ColorResources.getGreyColor(context)),
+              ),
             ),
-          ),
-          Padding(
-            padding: const EdgeInsets.only(
-                left: Dimensions.PADDING_SIZE_DEFAULT,
+            SizedBox(height: 50),
+            Padding(
+              padding: const EdgeInsets.all(Dimensions.PADDING_SIZE_DEFAULT),
+              child: CustomButton(
+                btnTxt: getTranslated('login', context),
+                onTap: () {
+                  Navigator.of(context).pushReplacement(
+                      MaterialPageRoute(builder: (_) => LoginScreen()));
+                },
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(
+                  left: Dimensions.PADDING_SIZE_DEFAULT,
+                  right: Dimensions.PADDING_SIZE_DEFAULT,
+                  bottom: Dimensions.PADDING_SIZE_DEFAULT,
+                  top: 12),
+              child: CustomButton(
+                btnTxt: getTranslated('signup', context),
+                onTap: () {
+                  Navigator.of(context).pushReplacement(
+                      MaterialPageRoute(builder: (_) => CreateAccountScreen()));
+                },
+                backgroundColor: Colors.black,
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(
+                left: Dimensions.FONT_SIZE_DEFAULT,
                 right: Dimensions.PADDING_SIZE_DEFAULT,
                 bottom: Dimensions.PADDING_SIZE_DEFAULT,
-                top: 12),
-            child: CustomButton(
-              btnTxt: getTranslated('signup', context),
-              onTap: () {
-                Navigator.of(context).pushReplacement(
-                    MaterialPageRoute(builder: (_) => CreateAccountScreen()));
-              },
-              backgroundColor: Colors.black,
-            ),
-          ),
-        ],
+                top: 12,
+              ),
+              child: CustomButton(
+                onTap: () {
+                  authProvider.googleSignIn();
+                },
+                inactiveColor: ColorResources.COLOR_WHITE,
+                btnTxt: 'NA',
+                child: Container(
+                  padding: EdgeInsets.all(Dimensions.PADDING_SIZE_SMALL),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      SvgPicture.asset(Images.google_logo),
+                      SizedBox(
+                        width: 15,
+                      ),
+                      Text(
+                        'Sign in with Google',
+                        style: TextStyle(
+                          color: ColorResources.getAccentColor(context),
+                          fontSize: 16,
+                          fontWeight: FontWeight.w500,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
